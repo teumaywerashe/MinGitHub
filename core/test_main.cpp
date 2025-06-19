15ec723aa505f8c2e0a38136305b87afd045fed8
teumaywerashe <teumay21@gmail.com>
2025-06-19 20:11:28 +0300

    Add files via upload

---
+++ core/test_main.cpp
@@ -0,0 +1,11 @@
+#include "blobManager.h"
+#include <fstream>
+#include <iostream>
+using namespace std;
+int main() {
+    BlobManager bm;
+    bm.saveBlob("example.txt");
+    cout << "Blob saved.\n";
+    return 0;
+}
+
